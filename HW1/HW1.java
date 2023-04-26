8157b5b9b980e4d649971bc352e7a18a4cc91220
LERA <111573064+Lera1811@users.noreply.github.com>
2023-04-26 12:24:12 +0300

    Add files via upload

---
+++ HW1/HW1.java
@@ -0,0 +1,44 @@
+// Реализация пирамидальной сортировки на Java
+import java.util.Arrays;
+public class Main {
+    public static void sort(int[] arr) {
+        int n = arr.length;
+
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        // One by one extract an element from heap
+        for (int i = n - 1; i >= 0; i--) {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // Call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+
+    private static void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        if (left < n && arr[left] > arr[largest])
+            largest = left;
+        if (right < n && arr[right] > arr[largest])
+            largest = right;
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 3, 15, 26, 17};
+        Main.sort(arr);
+        System.out.println(Arrays.toString(arr)); // [5, 6, 7, 11, 12, 13]
+    }
+}
